d0908d55037f22a8fd5b0ea992ad74a0720b8d40
amandixit576-jpg <amandixit576@gmail.com>
2026-03-07 21:03:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -445,6 +445,7 @@ else:
 
         with tab1:
             st.markdown("### 📊 Advanced Technical Chart")
+            data['SMA50'] = data['Close'].rolling(50).mean()
             data['20SMA'] = data['Close'].rolling(20).mean()
             data['Upper_BB'] = data['20SMA'] + 2 * data['Close'].rolling(20).std()
             data['Lower_BB'] = data['20SMA'] - 2 * data['Close'].rolling(20).std()
@@ -709,6 +710,7 @@ mega_footer = """
 </div>
 """
 st.markdown(mega_footer, unsafe_allow_html=True)
+
 
 
 
