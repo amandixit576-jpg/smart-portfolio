e6d3065f64125b99bbc1ed6591d34afcbe168cd6
amandixit576-jpg <amandixit576@gmail.com>
2026-03-22 12:53:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -678,7 +678,7 @@ if st.session_state.current_view != "HOME":
                     else:
                         st.warning("Income Statement data not available.")
                 except Exception as e:
-                st.warning(f"Error fetching Income Statement: {e}")
+                    st.warning(f"Error fetching Income Statement: {e}")
             with stmt2:
                 try:
                 # Yahan hum 'quarterly_financials' fetch kar rahe hain
